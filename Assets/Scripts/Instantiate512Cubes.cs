880d0505e867918a7bc53fcbe95a693c8fc50be7
sarah niemiec <sroseniemiec@gmail.com>
2017-08-29 14:31:12 -0400

    first commit

---
+++ Assets/Scripts/Instantiate512Cubes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate512Cubes : MonoBehaviour {
+	public GameObject sampleCubePrefab;
+	GameObject[] sampleCube = new GameObject[512];
+	public float maxScale;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < sampleCube.Length; i++)
+		{
+			GameObject instanceOfSampleCube = (GameObject)Instantiate (sampleCubePrefab);
+			instanceOfSampleCube.transform.position = this.transform.position;
+			instanceOfSampleCube.transform.parent = this.transform;
+			instanceOfSampleCube.name = "Cubely" + i;
+			this.transform.eulerAngles = new Vector3 (0, -0.703125f * i, 0);
+			instanceOfSampleCube.transform.position = Vector3.forward * 100;
+			sampleCube [i] = instanceOfSampleCube;
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < sampleCube.Length; i++) 
+		{
+			if (sampleCube != null) 
+			{
+				sampleCube [i].transform.localScale = new Vector3(10, (Visualizer.samples [i] * maxScale) + 2, 10);
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/Visualizer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent (typeof (AudioSource))]
+
+public class Visualizer : MonoBehaviour {
+	AudioSource audioSource;
+	public static float[] samples = new float[512];
+
+
+	// Use this for initialization
+	void Start () {
+		//		GetComponent<AudioSource> ().GetSpectrumData (samples, channel, FFTWindow);
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetSpectrumAudioSource ();
+	}
+
+	void GetSpectrumAudioSource() 
+	{
+		audioSource.GetSpectrumData (samples, 0, FFTWindow.Blackman);	
+	}
+}
